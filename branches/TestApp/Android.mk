e0686118bdf6bd1b879d610da0fb4a90a6ba5a9c
kevinfg05 <kevinfg05@184db6b7-b84b-3ad2-9f2b-76fc40edd215>
2010-12-18 10:37:43 +0000

    Fanggang:commit test app.

---
+++ branches/TestApp/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := user
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := TestApp
+
+# can not be platform
+LOCAL_CERTIFICATE := shared
+
+# specify the jni lib, only for android with version greater than 1.5. not for OMS
+
+include $(BUILD_PACKAGE)
+
+# Use the following include to make our test apk.
+#include $(call all-makefiles-under,$(LOCAL_PATH))
+
