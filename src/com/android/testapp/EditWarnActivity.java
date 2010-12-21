66943acf837243ddbd4192e1c9bd4d1e8e4c7340
kevinfg05 <kevinfg05@184db6b7-b84b-3ad2-9f2b-76fc40edd215>
2010-12-21 12:00:13 +0000

    Fanggang:add edit warn feature.

--- src/com/android/testapp/EditWarnActivity.java
+++ src/com/android/testapp/EditWarnActivity.java
@@ -6,8 +6,10 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +21,8 @@ import android.widget.TimePicker;
 
 public class EditWarnActivity extends Activity {
     
+    public static final String WARN_ID = "warn_id";
+    
     private DatePicker mTriggerDatePicker;
     private TimePicker mTriggerTimePicker;
     private DatePicker mFinishDatePicker;
@@ -29,6 +33,10 @@ public class EditWarnActivity extends Activity {
     private Spinner mShowTypeSpinner;
     private Button mCommitButton;
     private Button mCancelButton;
+    private static final String[] PROJECTION = {"_id", "owner", "trigger", "repeat", "interval", "finish", "message",
+        "vibrate", "sound", "show_type", "intent_target", "intent_action", "intent_data", "checked"};
+    
+    private int mWarnID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +53,30 @@ public class EditWarnActivity extends Activity {
         mCommitButton = (Button)findViewById(R.id.commit);
         mCancelButton = (Button)findViewById(R.id.cancel);
         
+        if (getIntent().getAction().equals(Intent.ACTION_EDIT)) {
+            mWarnID = getIntent().getIntExtra(WARN_ID, -1);
+            if (mWarnID != -1) {
+                Uri uri = ContentUris.withAppendedId(Uri.parse("content://com.android.proxy.warn/warns"), mWarnID);
+                Cursor cursor = getContentResolver().query(uri, PROJECTION, null, null, null);
+                if (cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(cursor.getLong(2));
+                    mTriggerDatePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), 
+                            calendar.get(Calendar.DAY_OF_MONTH), null);
+                }
+                cursor.close();
+            }
+        }
+        
         mCommitButton.setOnClickListener(new View.OnClickListener() {
             
             public void onClick(View v) {
-                insertRecord();
+                if (getIntent().getAction().equals(Intent.ACTION_INSERT)) {
+                    insertRecord();
+                } else if (getIntent().getAction().equals(Intent.ACTION_EDIT)) {
+                    updateRecord();
+                }
                 finish();
             }
         });
@@ -76,9 +104,17 @@ public class EditWarnActivity extends Activity {
         values.put("vibrate", false);
         values.put("sound", false);
         values.put("show_type", mShowTypeSpinner.getSelectedItemPosition());
+        values.put("intent_target", "com.android.mms/.ui.ConversationList");
         values.put("intent_action", Intent.ACTION_VIEW);
         values.put("intent_data", "smsto:");
+        values.put("checked", false);
         getContentResolver().insert(Uri.parse("content://com.android.proxy.warn/warns"), values);
+    }
+    
+    private void updateRecord() {
+        if (mWarnID != -1) {
+            
+        }
     }
 
 }
