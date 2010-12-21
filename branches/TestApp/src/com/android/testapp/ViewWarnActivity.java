1b5b61088eeb2796a610ae8f643c2468b0125afa
kevinfg05 <kevinfg05@184db6b7-b84b-3ad2-9f2b-76fc40edd215>
2010-12-21 12:00:13 +0000

    Fanggang:add edit warn feature.

--- branches/TestApp/src/com/android/testapp/ViewWarnActivity.java
+++ branches/TestApp/src/com/android/testapp/ViewWarnActivity.java
@@ -1,22 +1,32 @@
 package com.android.testapp;
 
 import android.app.ListActivity;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 public class ViewWarnActivity extends ListActivity {
     
 	private static final String TAG = "ViewWarnActivity";
-	
+	private static final boolean DEBUG = true;
 	
     private WarnAdapter mWarnAdapter;
     private String[] projection = {"_id", "owner", "trigger", "message", "vibrate"};
+    private static final int CT_MENU_EDIT = 1;
+    private static final int CT_MENU_DELETE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,7 @@ public class ViewWarnActivity extends ListActivity {
         mWarnAdapter = new WarnAdapter(getApplicationContext(), cursor);
         setContentView(R.layout.view);
         getListView().setAdapter(mWarnAdapter);
+        registerForContextMenu(getListView());
     }
     
     private class WarnAdapter extends CursorAdapter {
@@ -40,6 +51,7 @@ public class ViewWarnActivity extends ListActivity {
 			TextView textView = (TextView)view;
 			textView.setText(cursor.getInt(0) + ":" + cursor.getString(3) 
 					+ "&" + Boolean.valueOf(cursor.getString(4)));
+			textView.setTag(cursor.getInt(0));
 		}
 
 		public View newView(Context context, Cursor cursor, ViewGroup parent) {
@@ -48,6 +60,52 @@ public class ViewWarnActivity extends ListActivity {
 			return v;
 		}
         
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+            ContextMenuInfo menuInfo) {
+        // TODO Auto-generated method stub
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, CT_MENU_EDIT, 0, "EDIT");
+        menu.add(0, CT_MENU_DELETE, 0, "DELETE");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        int warnID = (Integer)info.targetView.getTag();
+        LOGD("onContextItemSelected:" + warnID);
+        switch (item.getItemId()) {
+        case CT_MENU_EDIT:
+            editWarn(warnID);
+            break;
+        case CT_MENU_DELETE:
+            deleteWarn(warnID);
+            break;
+        }
+        return super.onContextItemSelected(item);
+    }
+    
+    private void deleteWarn(int id) {
+        Uri deleteUri = ContentUris.withAppendedId(Uri.parse("content://com.android.proxy.warn/warns"), id);
+        int deleteCount = getContentResolver().delete(deleteUri, null, null);
+        LOGD("delete:" + deleteCount);
+        mWarnAdapter.notifyDataSetChanged();
+    }
+    
+    private void editWarn(int id) {
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.putExtra(EditWarnActivity.WARN_ID, id);
+        intent.setClass(getApplicationContext(), EditWarnActivity.class);
+        startActivity(intent);
+    }
+    
+    private static void LOGD(String text) {
+        if (DEBUG) {
+            Log.d(TAG, text);
+        }
     }
 
 }
