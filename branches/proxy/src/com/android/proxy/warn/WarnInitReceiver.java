d863bbb47a337b1344e5c55a1b9cdbc9247f69f2
kevinfg05 <kevinfg05@184db6b7-b84b-3ad2-9f2b-76fc40edd215>
2010-12-26 16:33:16 +0000

    Kevin: add warn initialize on boot completed.

--- branches/proxy/src/com/android/proxy/warn/WarnInitReceiver.java
+++ branches/proxy/src/com/android/proxy/warn/WarnInitReceiver.java
@@ -3,13 +3,15 @@ package com.android.proxy.warn;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class WarnInitReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO Auto-generated method stub
-
+    	Log.d("WarnInitReceiver", "onReceive");
+    	WarnManager.getInstance(context).initWarns();
     }
 
 }
