f3f9e0f3f0f40e9067a8f73b9826d3e46b709859
kevinfg05 <kevinfg05@184db6b7-b84b-3ad2-9f2b-76fc40edd215>
2011-03-24 17:04:00 +0000

    kevin -v-此行及以下内容将会被忽略--
    M    src/com/android/testapp/PostRequestActivity.java
    M    src/com/android/testapp/TestActivity.java

--- branches/TestApp/src/com/android/testapp/PostRequestActivity.java
+++ branches/TestApp/src/com/android/testapp/PostRequestActivity.java
@@ -8,6 +8,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -56,10 +57,13 @@ public class PostRequestActivity extends Activity {
 		int action = mSpinner.getSelectedItemPosition();
 		switch (action) {
 		case 0:
-			queryRecord();
+//			queryRecord();
+			FirstRequest(-1);
 			break;
 		case 1:
-			postRecord();
+			postRecord2();
+//			postRecord();
+//			insertShiwu();
 			break;
 		case 2:
 			deleteRecord();
@@ -91,6 +95,96 @@ public class PostRequestActivity extends Activity {
 		}
 	}
 	
+	private void insertShiwu() {
+		Request request = new Request();
+		request.action = Request.ACTION_POST;
+    	request.items = "AFFAIRS";
+    	request.versionId = "-1";
+    	request.packageName = getPackageName();
+    	String body = "<?xml version='1.0' encoding='utf-8'?>"+
+    				 "<OBJECTS><OBJECT>"+
+    				 "<USERID>" + "androidproxy007" + "</USERID>"+
+    				 "<AFFAIRSID>"+0000001+"</AFFAIRSID>"+
+    				 "</OBJECT></OBJECTS>";
+    	request.body = body;
+    	if (TestActivity.mIsBinding && TestActivity.mService != null) {
+    		try {
+				Response response = TestActivity.mService.postRequest(request, true);
+//				Toast.makeText(getApplicationContext(), response.body, Toast.LENGTH_LONG).show();
+				showDialog(response.body);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+	}
+	
+	private void FirstRequest(int localVersion) {
+    	//Utils.LOGD( "--------goto FirstRequest---------");
+    	Request request = new Request();
+    	request.action = Request.ACTION_GET;
+    	request.items = "CONTACES";
+    	//request.versionId = String.valueOf(localVersion);
+    	request.versionId = "-1";
+    	//request.packageName = getPackageName();
+    	request.packageName = "com.android.testapp";
+    	request.body = "<?xml version='1.0' encoding='utf-8'?><OBJECTS><OBJECT>" +
+    			"<RESOURCEID>"+request.items+"</RESOURCEID>"+//必须
+    			"<CONTACESID>"+ 1 +"</CONTACESID></OBJECT></OBJECTS>";
+//    	Utils.requestLog(request);//log
+    	
+    	if (TestActivity.mIsBinding && TestActivity.mService != null) {
+    		try {
+				Response response = TestActivity.mService.postRequest(request, true);
+				if(response.body!=null){
+//					stringXML2(response.body.toString());//第一次有控制针
+//					Utils.LOGD( "response.body================================"+response.body.toString());
+					//showDialog(response.body);
+					Log.d("PostRequestActivity", "response bodyyyyyyyyyyyyyyyyyyy");
+					Log.d("PostRequestActivity", response.body);
+				}
+
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+    	}
+    } 
+
+	
+	private void postRecord2() {
+
+    	Request request = new Request();
+    	request.action = Request.ACTION_POST;
+    	request.items = "CONTACES";
+    	request.versionId = "-1";
+    	request.packageName = getPackageName();
+    	request.body = "<?xml version='1.0' encoding='utf-8'?><OBJECTS><OBJECT><CONTACESID>eaglelink000009900" +
+        "</CONTACESID><WHETHEREXTEND>1</WHETHEREXTEND><FAMILYNAME>江</FAMILYNAME><USERNAME>晓亮" +
+        "</USERNAME><NICKNAME>jenny</NICKNAME><HEADPORTRAIT>pic1.jpg</HEADPORTRAIT><COMPANYNAME>" +
+        "移通讯联</COMPANYNAME><DEPARTMENT>研发部</DEPARTMENT><ADDRESS>嘉泰国际B座3</ADDRESS>" +
+        "<TELEPHONEEXCHANGE>85711780</TELEPHONEEXCHANGE><EXTENSION>8301</EXTENSION><FAX></FAX>" +
+        "<MOBILEPHONE1>13426341648</MOBILEPHONE1><MOBILEPHONE2></MOBILEPHONE2><TELEPHONE>" +
+        "0739-8811332</TELEPHONE><EMAIL>xiaoliang.jiang@eaglelink.cn</EMAIL><QQ>12653373</QQ>" +
+        "<MSN></MSN><BIRTHDAY>1986--28</BIRTHDAY><HOBBY>打篮球，编程，电脑</HOBBY><NATIVEPLACE>" +
+        "湖南</NATIVEPLACE><SCHOOLNAME1>长沙理工大学</SCHOOLNAME1><MEMOSID></MEMOSID><SPARE1>扩展一" +
+        "</SPARE1><SPARE2>扩展二</SPARE2><ATTRIBUTE NAME='ff'>f1</ATTRIBUTE>" +
+        "<ATTRIBUTE NAME='qqq'>qq1</ATTRIBUTE></OBJECT></OBJECTS>";
+    	
+    	if (TestActivity.mIsBinding && TestActivity.mService != null) {
+    		try {
+				Response response = TestActivity.mService.postRequest(request, true);
+//				Toast.makeText(getApplicationContext(), response.body, Toast.LENGTH_LONG).show();
+				showDialog(response.body);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	
+	}
+	
 	private void postRecord() {
     	Request request = new Request();
     	request.action = Request.ACTION_POST;
@@ -108,7 +202,7 @@ public class PostRequestActivity extends Activity {
     		"</OBJECT></OBJECTS>";
     	if (TestActivity.mIsBinding && TestActivity.mService != null) {
     		try {
-				Response response = TestActivity.mService.postRequest(request);
+				Response response = TestActivity.mService.postRequest(request, true);
 //				Toast.makeText(getApplicationContext(), response.body, Toast.LENGTH_LONG).show();
 				showDialog(response.body);
 			} catch (RemoteException e) {
@@ -124,11 +218,11 @@ public class PostRequestActivity extends Activity {
     	request.items = "CONTACES";
     	request.versionId = "-1";
     	request.packageName = getPackageName();
-    	request.body = "<?xml version='1.0' encoding='utf-8'?><OBJECTS><OBJECT><CONTACESID>"+ mIDEdit.getText().toString() +
+    	request.body = "<?xml version='1.0' encoding='utf-8'?><OBJECTS><OBJECT><CONTACESID>" + "eaglelink000009900" +
     		"</CONTACESID></OBJECT></OBJECTS>";
     	if (TestActivity.mIsBinding && TestActivity.mService != null) {
     		try {
-				Response response = TestActivity.mService.postRequest(request);
+				Response response = TestActivity.mService.postRequest(request, true);
 //				Toast.makeText(getApplicationContext(), response.body, Toast.LENGTH_LONG).show();
 				showDialog(response.body);
 			} catch (RemoteException e) {
@@ -148,7 +242,7 @@ public class PostRequestActivity extends Activity {
     		+ mIDEdit.getText().toString() +"</CONTACESID></OBJECT></OBJECTS>";
     	if (TestActivity.mIsBinding && TestActivity.mService != null) {
     		try {
-				Response response = TestActivity.mService.postRequest(request);
+				Response response = TestActivity.mService.postRequest(request, true);
 //				Toast.makeText(getApplicationContext(), response.body, Toast.LENGTH_LONG).show();
 				showDialog(response.body);
 			} catch (RemoteException e) {
@@ -175,7 +269,7 @@ public class PostRequestActivity extends Activity {
 		"</OBJECT></OBJECTS>";
     	if (TestActivity.mIsBinding && TestActivity.mService != null) {
     		try {
-				Response response = TestActivity.mService.postRequest(request);
+				Response response = TestActivity.mService.postRequest(request, true);
 //				Toast.makeText(getApplicationContext(), response.body, Toast.LENGTH_LONG).show();
 				showDialog(response.body);
 			} catch (RemoteException e) {
